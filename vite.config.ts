ba2ea63ae2bc1c3a95818eff1b831d431dccc270
ayyarmm1-cmd <ayyar.mm1@gmail.com>
2026-09-24 01:35:05 +0630

    Update base path in Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/marketing_capsule_V1.2/',
+  base: '/Marketing_capsule_V1.2/',
 
   server: {
     port: 3000,
